36810aa707a68feb66b52f9d99496a4d1b7dfd44
yash1888 <101277644+yash1888@users.noreply.github.com>
2022-04-02 21:21:03 +0530

    Add files via upload

---
+++ 3_implementation/inc/function.h
@@ -0,0 +1,38 @@
+/**
+ * @file function.h
+ * @author Gulshan J Dharmaraj
+ * @brief Handcricket game function file
+ * @version 0.1
+ * @date 2021-11-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+
+void display_bat(int,int);
+void display_bowl(int ,int);
+int user_bat2(int );
+int user_bowl2(int);
+int com_bat();
+int com_bowl();
+int com_user_bat(int);
+int com_user_bowl(int);
+int user_bat();
+int user_bowl();
+int toss(int,int);
+  
+
+  
+  
+  
+  
+  
+  #endif
